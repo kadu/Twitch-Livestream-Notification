5b244dc116fb311f1b67a9a1b0650c4495e90714
juliallabs <laboratoriodajulia@gmail.com>
2022-11-25 21:07:10 -0300

    git ignore att

--- src/credencials.h
+++ src/credencials.h
@@ -1,2 +1,2 @@
-const String clientId = "j1p2uk6urztlq6b7z8t5vxd8c8b0qo";
-const String clientSecret = "1e8jla29eijcr2sgj5o2ju9gdhg74x";
+const String clientId = "";
+const String clientSecret = "";
