a89cd1137d4667d35b72fc09cfa3ee40784ed08a
BiancaRerre <52195618+BiancaRerre@users.noreply.github.com>
2022-12-11 20:14:45 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,126 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-#include <WiFiClientSecure.h>
-#include <Arduino_JSON.h>
 #include <Adafruit_NeoPixel.h>
-#include <ESP8266mDNS.h>   
+#ifdef ESP32
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#else
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#endif
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+	
+#include <cstring>
 #include <sstream>
 #include <string>
-#include <cstring>
-#include "credencials.h"
-#include "HTML.h"
 
+#include "./HTML.h"
+#include "./style.h"
+#include "./js.h"
+#include "./twitch.hpp"
 
-
-// twitch connection
-const char* host = "id.twitch.tv";
-const uint16_t port = 443;
-String access_token = "";
+// LED config
+#define PIN 4
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int status = 0;
 
 // WifiManager config
-void wmConfig(){
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+void wmConfig() {
+    // WiFiManager, Local intialization. Once its business is done, there is no
+    // need to keep it around
     WiFiManager wm;
-    //wm.resetSettings(); // reset the wifi config
-    
+    // wm.resetSettings(); // reset the wifi config
+
     bool res;
-    res = wm.autoConnect("StarON"); // password protected ap
- 
-    if(!res) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
-    else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+    res = wm.autoConnect("StarON");  // password protected ap
+
+    if (!res) {
+        // Serial.println("Failed to connect");
+        //  ESP.restart();
+    } else {
+        // if you get here you have connected to the WiFi
+        // Serial.println("connected...yeey :)");
     }
-        Serial.println(WiFi.localIP());
+    // Serial.println(WiFi.localIP());
 }
 
 // WebServer config
-ESP8266WebServer server(80); // Set web server port number to 80
-String streamerName="";
-String COLOR="";
+#ifdef ESP32
+WebServer server(80);  // Set web server port number to 80
+#else
+ESP8266WebServer server(80);  // Set web server port number to 80
+#endif
+String streamerName = "";
+String cor = "";
+int corR = 0;
+int corG = 0;
+int corB = 0;
 
-void hendleIndex() {  // send HTML to the page
-    server.send(200, "text/html", postForms); // check HTML.h file
+void hendleIndex() {                           // send HTML to the page
+    Serial.println("GET /");
+    server.send(200, "text/html", postForms);  // check HTML.h file
 }
+
+void handleStyle() {                           // send HTML to the page
+    Serial.println("GET /style.css");
+    server.send(200, "text/css", style);  // check HTML.h file
+}
+
+void handlejs() {                           // send HTML to the page
+    Serial.println("GET /js");
+    server.send(200, "application/javascript", js);  // check HTML.h file
+}
+
+
+void handleStatus() {                           // send JSON to the page
+//jsonstatus = "[{\"canal\":\""+streamerName+"\",\"color\":\""+cor+"\",\"status\":\""+status+"\"}]";   
+    Serial.println("GET /staus");
+    server.send(200, "application/json", "[{\"canal\":\""+streamerName+"\",\"color\":\""+cor+"\",\"status\":\""+status+"\"}]"); 
+}
+
 
 void handleGetParam() {
 
     if (server.hasArg("STREAMER")) {
-        streamerName = server.arg("STREAMER"); // get the streamer name and put on the streamerName variable
+        streamerName = server.arg("STREAMER");  // get the streamer name and put
+                                                // on the streamerName variable
     }
-    if (server.hasArg("COLOR")) {
-        streamerName = server.arg("COLOR"); // get the COLOR
+    if (server.hasArg("cor")) {
+        cor = server.arg("cor");  // get the COLOR
     }
 
-    Serial.print("Stremaer Name - ");
-    Serial.println(streamerName);
+
+     if (server.hasArg("r")) {
+        corR = server.arg("r").toInt();  // get the COLOR
+        Serial.println(corR);
+    }
+     if (server.hasArg("g")) {
+        corG = server.arg("g").toInt();  // get the COLOR
+        Serial.println(corG);
+    }
+
+ if (server.hasArg("b")) {
+        corB = server.arg("b").toInt();  // get the COLOR
+        Serial.println(corB);
+    }
+
+
+
+
+    for (int i = 0; i < 3; i++) {
+        pixels.setPixelColor(0, corR, corG, corB);
+        pixels.show();
+        delay(200);
+        pixels.clear();
+        pixels.show();
+        delay(200);
+    }
+  Serial.println("GET /getname");
+    Serial.print("Streamer: ");
+    Serial.print(streamerName);
+    Serial.print(" - ");
+    Serial.print("color: " + cor + " rgb("+server.arg("r")+", "+server.arg("g")+", "+server.arg("b")+")");
+    Serial.println("");
+
 }
 
 void handleNotFound() {
@@ -72,160 +135,63 @@ void handleNotFound() {
     for (uint8_t i = 0; i < server.args(); i++) {
         message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
     }
-    server.send(404, "text/plain", message);
+    server.send(200, "text/html", message);  // check HTML.h file
 }
-
-
-//LED config
-#define PIN 2
-#define NUMPIXELS 2 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-
 
 void setup() {
-    WiFi.mode(WIFI_STA); 
+    WiFi.mode(WIFI_STA);
     Serial.begin(115200);
     wmConfig();
-     if (!MDNS.begin("starOn")) {             // Start the mDNS responder for esp8266.local
-    Serial.println("Error setting up MDNS responder!");
+    if (!MDNS.begin("starOn")) {  // Start the mDNS responder for esp8266.local
+        Serial.println("Error setting up MDNS responder!");
     }
-   Serial.println("mDNS responder started");
-   MDNS.addService("http", "tcp", 80);
-   MDNS.notifyAPChange();
-   WiFi.hostname("starOn");
+    // Serial.println("mDNS responder started");
+    MDNS.addService("http", "tcp", 80);
+    WiFi.hostname("starOn");
 
     server.on("/", hendleIndex);
-    server.on("/getName", handleGetParam);
+    server.on("/getname", handleGetParam);
+    server.on("/style.css", handleStyle);
+    server.on("/status", handleStatus);
+    server.on("/js", handlejs);
     server.onNotFound(handleNotFound);
     server.begin();
-    Serial.println("HTTP server started");
+    // Serial.println("HTTP server started");
 
-    //LED
+    // LED
+
     pixels.begin();
     pixels.clear();
-    delay(500);
-   
 }
 
+uint32_t lasTimeUpdateLed;
 
- 
 void loop() {
-  server.handleClient();
-  WiFiClientSecure client;
-  client.setInsecure();
-  MDNS.update();
-  if(streamerName != ""){
+#ifndef ESP32
+    MDNS.update();
+#endif
 
-  if (!client.connect(host, port)) {
-    Serial.println("1 connection failed");
-    Serial.println("wait 5 sec...");
-    delay(5000);
-    return;
-  }  
+    server.handleClient();
 
-  String data = "client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=client_credentials";
-  
-//   This will send the request to the server
-  client.println("POST /oauth2/token HTTP/1.1");
-  client.println("Host: id.twitch.tv");
-  client.println("Content-Type: application/x-www-form-urlencoded");
-  client.println("Content-Length: " + String(data.length()));
-  client.println();
-  client.println(data);
-
-
-  //read back one line from server
-  Serial.println("receiving from remote server");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
+    if (streamerName != "" && (millis() - lasTimeUpdateLed) > 300) {
+        // Serial.println("Recebendo stream data");
+        // Serial.println(response);
+        if (streamerIsOn(streamerName)) {
+            server.handleClient();
+            pixels.setPixelColor(0, corR, corG, corB); //definir cor dos leds
+            pixels.show(); //aplicar alterações nos leds
+            Serial.println("TA ON");
+            status = 1;
+        } else {
+            server.handleClient();
+            pixels.clear();//definir que quer apagar leds
+            pixels.show(); //aplicar alterações nos leds
+            Serial.println("TA OFF");
+            status = 0;
+        }
+        lasTimeUpdateLed = millis();
+        // Serial.println("wait 5 sec...");
+        // delay(5000);
     }
-  }
-
-  std::stringstream ss;
-  bool capturing = false;
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    if(ch == '{' || capturing == true || ch == '}') {
-      capturing = true;
-      ss << ch;
-      if(ch == '}') {
-        capturing = false;
-      }
-    }    
-  }  
-
-  JSONVar json = JSON.parse(ss.str().c_str());  
-
-  access_token = String((const char*) json["access_token"]);
-  Serial.println(access_token);
-  
-  if (!client.connect("api.twitch.tv", port)) {
-    Serial.println("2 connection failed");
-    Serial.println("wait 5 sec...");
-    delay(5000);
-    return;
-  }  
-
-  Serial.println("GET /helix/streams?user_login="+streamerName+ " HTTP/1.1");
-  // This will send the request to the server
-  client.println("GET /helix/streams?user_login="+streamerName+ " HTTP/1.1");
-  client.println("Host: api.twitch.tv"); 
-  client.println("Authorization: Bearer " + access_token);
-  client.println("Client-Id: " + clientId);
-  client.println();
-  client.println();
-
-  timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 10000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
-    }
-  }
-  ss.clear();
-
-  delay(1000);
-
-  capturing = false;
-  String response = "";
-  while (client.available()) {    
-    char ch = static_cast<char>(client.read());
-    if(ch == '{' || capturing == true || ch == '}') {
-      capturing = true;
-      response.concat(ch);
-      if(ch == '}') {
-        capturing = false;
-      }
-    }
-  }
-
-  Serial.println("Recebendo stream data");
-  Serial.println(response);
-  if (response.length() > 27){
-    pixels.setPixelColor(1, pixels.Color(254, 0, 153));
-    pixels.show();   
-    Serial.println("TA ON");
-  }else{
-     pixels.setPixelColor(1, pixels.Color(0, 0, 255));
-     pixels.show(); 
-    Serial.println("TA OFF");
-    
-  }
-
-
-
-  Serial.println("closing connection");
-  client.stop();
-
-  Serial.println("wait 5 sec...");
-  delay(5000);
-  }
-  }
+ 
+}
